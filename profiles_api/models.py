e728d7da93cc47bd4add714254112ead7d34e3cd
Carlos8 <cgarcia@centrogeo.edu.mx>
2020-10-05 19:01:38 -0500

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email adress! :3')
+
+        else:
+            #Pone en minuscula
+            email=self.normalize_email(email)
+            user= self.model(email=email,name=name)
+            #Password encriptado, no en texto---> Se vera hasheado
+            user.set_password(password)
+            #Guardar usuario #base de datos estandar
+            user.save(using=self._db)
+
+            return (user)
+
+    def create_superuser(self,email,name,password):
+        """Create new super user"""
+        # Reusar la funcion create_user
+        user= self.create_user(email,name,password)
+
+        #is_superuser creado por PermissionMixin
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return(user)
+
+
+
+
+
+#Clase usuario ---> Esta es la nueva manera (en vez de la de Django) para manejar usuarios
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    #correo
+    email = models.EmailField(max_length=255, unique=True)
+    #Nombre
+    name = models.CharField(max_length=255)
+    #Esta conectado
+    is_active = models.BooleanField(default=True)
+    #Puede acceder al admin?
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    #No esta mal, email sera id
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Return full name of user"""
+
+        return (self.name)
+
+    def get_short_name(self):
+        """Return short name of user"""
+        return (self.name)
+
+    #Lo que se regresa
+    def __str__(self):
+        """Return string representation of the user"""
+
+        return (self.email)
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Cambiar el default de user manager al creado en models.py IMPORTANTE
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
